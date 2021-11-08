89468b53cba064a8ef85779e943f3a09fd19cfbc
Darwin Gonzalez <8992534+DwGonzalez@users.noreply.github.com>
2021-11-08 02:00:30 -0400

    ✨ Fixed input. Added cors API. Added upvotes & author.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Post from './components/Post';
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [subreddit, setSubreddit] = useState('pcmasterrace');
+  const [subreddit, setSubreddit] = useState('webdev');
 
   useEffect(() => {
-    fetch('https://www.reddit.com/r/pcmasterrace.json').then(res => {
-      if (res.status != 200) {
+    fetch('https://can-cors.herokuapp.com/https://www.reddit.com/r/' + subreddit + '.json').then(res => {
+      if (res.status !== 200) {
         console.log("ERROR");
         return;
       }
@@ -23,11 +23,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input type="text" className="input" value="pcmasterrace" />
+        <input type="text" className="input" value={subreddit} onChange={(e => setSubreddit(e.target.value))} />
       </header>
       <div className="post_list">
         {
-          (posts != null) ? posts.map((post, index) => <Post key={index} post={post.data} />) : ''
+          (posts !== null) ? posts.map((post, index) => <Post key={index} post={post.data} />) : 'Not Found'
         }
       </div>
     </div>
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,8 +3,10 @@ import React from 'react';
 function Post(props) {
     return (
         <article>
-            <a href={"https://reddit.com" + props.post.permalink} target="_blank">
+            <a href={"https://reddit.com" + props.post.permalink} rel="noopener">
                 <h3>{props.post.title}</h3>
+                <h5>By {props.post.author}</h5>
+                <h6>Upvotes: <u>{props.post.ups}</u></h6>
             </a>
         </article>
     )
